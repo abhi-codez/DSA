5caa37376e792cc7f89c49e02c77294f40c79655
abhi-codez <ak1241039@gmail.com>
2026-09-08 18:27:50 +0530

    Time: 0 ms (100%), Space: 12.8 MB (5.3%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;          // last element of nums1
-        int j = n - 1;          // last element of nums2
-        int k = m + n - 1;      // last position of nums1
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            }
-            else {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
+        
+        vector<int> res(m + n);
+       int i=0, j=0, k=0;
+       while(i<m && j<n){
+        if(nums1[i]<=nums2[j]){
+            res[k]=nums1[i];
+            k++;
+            i++;
         }
-
-        // If elements are remaining in nums2
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
+        else{
+            res[k]=nums2[j];
+            k++;
+            j++;
         }
+       }
+        while(j<n){
+            res[k]=nums2[j];
+            k++;
+            j++;
+        }
+        while(i<m)
+        {
+            res[k]=nums1[i];
+            k++;
+            i++;
+        
+       } 
+       nums1=res;
     }
 };
